06eba510672e996d829468655890d5455445ddbc
Lucas Tonial Scortegagna <ltonial@gmail.com>
2025-05-06 20:28:21 -0300

    feat - MazeRunner Tutorial - first steps

---
+++ Assets/Scripts/LavaOffsetAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LavaOffsetAnimation : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Renderer _renderer;
+    private Vector2 _offset;
+  
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _offset = _renderer.material.GetTextureOffset("_BaseMap");
+    }
+  
+    void Update()
+    {
+        _offset.y += _speed * Time.deltaTime;
+        _renderer.material.SetTextureOffset("_BaseMap", _offset);
+    }
+}
---
+++ Assets/Scripts/SmoothPlatformMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SmoothPlatformMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _speed;
+    
+    private Vector3[] _targetPosition;
+    private int _idTarget;
+    
+    private void Start()
+    {
+        _targetPosition = new Vector3[2];
+        _targetPosition[0] = _target.position;
+        _targetPosition[1] = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+            _targetPosition[_idTarget], _speed * Time.deltaTime);
+
+
+        if (transform.position == _targetPosition[_idTarget])
+        {
+            _idTarget =_idTarget < 1 ? _idTarget + 1 : 0;
+        }
+    }
+}
